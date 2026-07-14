d1b62560a0b0f5caf13b45cd595dbbadda189a1a
roniarosariosamk <roniarosario1606@gmail.com>
2026-07-15 01:32:53 +0530

    Use lightweight enbedding model

--- src/embeddings.py
+++ src/embeddings.py
@@ -2,14 +2,12 @@ import streamlit as st
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
-@st.cache_resource
+@st.cache_resource(show_spinner=False)
 def load_embedding_model():
     """
-    Load the Hugging Face embedding model only once.
+    Loads embedding model only once.
     """
 
-    embedding = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
     )
-
-    return embedding
